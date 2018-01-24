30d74a5fa10382fb0b33a4479b35ee5c932b7594
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2018-01-24 03:20:08 +0000

    My first file

---
+++ lab01/lab01.cpp
@@ -0,0 +1,17 @@
+//Created By: Isaac Vander Sluis
+//Created On: 1/23/17
+
+/* 
+
+Lo, I have made a file.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+    
+    return 0;
+}
