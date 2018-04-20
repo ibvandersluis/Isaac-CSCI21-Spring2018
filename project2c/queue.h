273f99985e1034b32d86b2a8fae0e4b00caa82da
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2018-04-20 03:09:47 +0000

    Project 2C Submission - IN PROGRESS

--- project2c/queue.h
+++ project2c/queue.h
@@ -1,1 +1,19 @@
 #include "node.h"
+
+template<typename T>;
+class Queue {
+    public:
+        Queue();                                                                // Default Constructor
+        ~Queue();                                                               // Destructor
+        
+        void set_head(Node* address);                                           // Mutator
+        void set_tail(Node* address);                                           // Mutator
+        
+        void push();                                                            // Adds an item to the queue
+        void pop();                                                             // Removes an item from the queue
+        void print();                                                           // Prints all items in the queue
+        unsigned int size();                                                    // Returns the number of items in the queue
+    private:
+        Node* head_;                                                            // Points to first node in stack
+        Node* tail_;                                                            // Points to last node in stack
+};
--- project2c/stack.h
+++ project2c/stack.h
@@ -1,1 +1,19 @@
 #include "node.h"
+
+template<typename T>;
+class Stack {
+    public:
+        Stack();                                                                // Default Constructor
+        ~Stack();                                                               // Destructor
+    
+        void set_head(Node* address);                                           // Mutator
+        void set_tail(Node* address);                                           // Mutator
+        
+        void push();                                                            // Adds an item to the stack
+        void pop();                                                             // Removes an item from the stack
+        void print();                                                           // Prints all items in the stack
+        unsigned int size();                                                    // Returns the number of items in the stack
+    private:
+        Node* head_;                                                            // Points to first node in stack
+        Node* tail_;                                                            // Points to last node in stack
+};
