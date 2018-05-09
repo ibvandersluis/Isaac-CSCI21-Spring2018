2f8224c5f6c4ff848e297cfe4380ce2d690920db
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2018-05-09 03:11:03 +0000

    Tree

---
+++ tree/tree.cpp
@@ -0,0 +1,18 @@
+#include "tree.h"
+
+unsigned int Tree::size() {
+    return size_;
+}
+
+void Tree::printTree() {
+    
+}
+
+void Tree::insert(string word, unsigned int weight) {
+    
+}
+
+
+void Tree::remove(string word) {
+    
+}
